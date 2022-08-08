e11bc51295530eac90f5565fbeddb18acd201f6a
Diego Villamizar <phicar@yashira.org>
2022-08-08 14:54:34 -0500

    Update and rename hola (1).py to InsercionYpermutacionesAgosto5.py

--- InsercionYpermutacionesAgosto5.py
+++ InsercionYpermutacionesAgosto5.py
@@ -43,34 +43,3 @@ def Cycles(A):
 		#V[i]=True
 		B.append(C)
 	return B
-
-			
-	
-n =0 # tamaño del arreglo https://sites.google.com/view/dvillami/algoritmos
-P = [] #Permutacion
-A = [] #arreglo a ordenar
-B = [] #arreglo ordenado
-V = [] #arreglo de visitados
-def perm(it):
-	global P,B,n,V,A
-	if it==n:
-		#VERIFICAR que la permutacion ordene
-		print(P,Cycles(P))
-		return
-	for j in range(n):
-		if  not V[j]:
-			P[it]=j #it:--->j
-			V[j]= True
-			perm(it+1)
-			V[j]=False
-			
-def sort():
-	global n,P,B,V,A #referenciar globales
-	n = len(A)
-	P = [0 for i in range(n)]
-	B = [0 for i in range(n)]
-	V = [False for i in range(n) ]
-	perm(0)#vamos a iterar sobre las permutaciones
-	return B 
-A =[5,32,1]
-#sort()
